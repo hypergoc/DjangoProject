449fe0ce9b64147648c0fe6c9d005e0d60fc6c34
goc <hypergoc@gmail.com>
2025-09-05 19:46:16 +0200

    WIP IG pull api DONE

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,10 +1,43 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import InstagramPost
 
 @admin.register(InstagramPost)
 class InstagramPostAdmin(admin.ModelAdmin):
-    list_display = ('instagram_id', 'publish_date', 'published')
+    list_display = ('instagram_id', 'publish_date', 'like_count', 'comment_count', 'published', 'image_preview_list')
     list_filter = ('published', 'publish_date')
-    search_fields = ('instagram_id', 'content', 'post_song')
+    search_fields = ('instagram_id', 'content')
     list_editable = ('published',)
-    date_hierarchy = 'publish_date'
+    
+    readonly_fields = (
+        'instagram_id', 'content', 'post_url', 'publish_date', 'post_song', 
+        'like_count', 'comment_count', 'image_preview_item'
+    )
+    
+    fieldsets = (
+        ('Status', {
+            'fields': ('published',)
+        }),
+        ('Informacije o objavi', {
+            'fields': ('instagram_id', 'post_url', 'publish_date', 'like_count', 'comment_count')
+        }),
+        ('Sadržaj', {
+            'fields': ('content', 'post_song', 'image_preview_item')
+        }),
+    )
+
+    def image_preview_list(self, obj):
+        """Prikaz male slike u listi (grid view)."""
+        if obj.post_image:
+            # Koristimo .url atribut FileField-a
+            return format_html('<a href="{0}" target="_blank"><img src="{0}" style="height: 60px; width: auto;" /></a>', obj.post_image.url)
+        return "Nema slike"
+    image_preview_list.short_description = 'Slika'
+
+    def image_preview_item(self, obj):
+        """Prikaz veće slike u formi za uređivanje (item view)."""
+        if obj.post_image:
+            # Koristimo .url atribut FileField-a
+            return format_html('<a href="{0}" target="_blank"><img src="{0}" style="max-height: 300px; max-width: 300px;" /></a>', obj.post_image.url)
+        return "Nema slike"
+    image_preview_item.short_description = 'Pregled slike'
--- instagram/models.py
+++ instagram/models.py
@@ -1,18 +1,22 @@
 from django.db import models
 
 class InstagramPost(models.Model):
-    instagram_id = models.CharField(max_length=100, unique=True)
-    content = models.TextField(blank=True, null=True)
-    post_url = models.URLField(max_length=256, unique=True)
-    post_image = models.URLField(max_length=512)
-    published = models.BooleanField(default=False)
-    publish_date = models.DateTimeField()
-    post_song = models.CharField(max_length=512, blank=True, null=True)
-
-    def __str__(self):
-        return f"Post {self.instagram_id}"
+    instagram_id = models.CharField(max_length=255, unique=True, verbose_name="Instagram ID")
+    content = models.TextField(blank=True, null=True, verbose_name="Opis")
+    post_url = models.URLField(max_length=500, blank=True, verbose_name="URL objave")
+    # Promjena: URLField je postao FileField za lokalno spremanje slika
+    post_image = models.FileField(upload_to='IG/', max_length=512, blank=True, verbose_name="Slika")
+    publish_date = models.DateTimeField(null=True, blank=True, verbose_name="Datum objave")
+    published = models.BooleanField(default=False, verbose_name="Objavljeno")
+    post_song = models.CharField(max_length=255, blank=True, null=True, verbose_name="Pjesma")
+    
+    comment_count = models.PositiveIntegerField(default=0, verbose_name="Broj komentara")
+    like_count = models.PositiveIntegerField(default=0, verbose_name="Broj lajkova")
 
     class Meta:
-        verbose_name = "Instagram Post"
-        verbose_name_plural = "Instagram Posts"
         ordering = ['-publish_date']
+        verbose_name = "Instagram objava"
+        verbose_name_plural = "Instagram objave"
+
+    def __str__(self):
+        return f"Objava {self.instagram_id} od {self.publish_date.strftime('%Y-%m-%d') if self.publish_date else 'N/A'}"
