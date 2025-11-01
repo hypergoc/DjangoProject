68a69c98a0e4009b8c889284f7f9e3b6fd095a8c
goc <hypergoc@gmail.com>
2025-11-01 07:08:07 +0100

    wip - fblinkapi

--- fblinkapi/admin.py
+++ fblinkapi/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Post
+from django.utils.html import format_html
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'image_tag', 'published_at')
+
+    def image_tag(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="max-width: 150px; height: auto;" />'.format(obj.image.url))
+        return "No Image"
+    image_tag.short_description = 'Image'
+
+
+admin.site.register(Post, PostAdmin)
--- fblinkapi/models.py
+++ fblinkapi/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    status = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    published_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
