8cbfaf1bdf9f4bc7f3b8c2e560f8aa52b26b3090
goc <hypergoc@gmail.com>
2025-11-15 06:24:27 +0100

    wip

--- apartman/admin.py
+++ apartman/admin.py
@@ -4,17 +4,18 @@
 from django.contrib import admin
 from .models import Apartman
 from price.models import Termin
+from bookingengine.models import Booking
 from datetime import date
 
 class TerminInline(admin.TabularInline):
-    """
-    Ova klasa govori Djangu: 'Hej, kad prikazuješ Apartman,
-    pokaži mi i tablicu za unos/editiranje Termina.'
-    """
     model = Termin
-    # Polja koja želiš vidjeti i editirati u tablici
     fields = ('date_from', 'date_to', 'value')
-    # 'extra = 1' ti daje jedan prazan red za unos novog termina
+    extra = 1
+    ordering = ('date_from',)
+
+class BookingInline(admin.TabularInline):
+    model = Booking
+    fields = ('date_from', 'date_to', 'price', 'visitors_count', 'approved')
     extra = 1
     ordering = ('date_from',)
 
@@ -23,21 +24,13 @@ class ApartmanAdmin(admin.ModelAdmin):
     list_display = ('naziv', 'company', 'size', 'capacity', 'get_todays_price')
     search_fields = ('naziv', 'company__name', 'opis')
     list_filter = ('company',)
-    # raw_id_fields is useful for ForeignKey fields with many options
     raw_id_fields = ('company',)
-    inlines = [TerminInline]
+    inlines = [TerminInline, BookingInline]
 
     def get_todays_price(self, obj):
-        """
-        Dohvaća cijenu za apartman za današnji dan iz Price modela.
-        """
         today = date.today()
-
-
-
         price_entry = Termin.objects.filter(apartman=obj, date_from__lte = today, date_to__gte=today).first()
         if price_entry:
             return price_entry.value
         return "N/A"
-
     get_todays_price.short_description = 'Cijena za danas'
